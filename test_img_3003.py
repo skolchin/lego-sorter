5fa5d3720a7bab028b6fc4f71f9e9eb0aa639390
kol <skolchin@gmail.com>
2022-12-04 22:35:24 +0300

    tests

--- test_img_3003.py
+++ test_img_3003.py
@@ -7,15 +7,20 @@ import img_utils22 as imu
 
 def main():
     pipe = imu.Pipe() | imu.LoadFile('./out/3003_test.png') \
-        | imu.ExtractObjects(bgcolor=255) | imu.ShowImage('image1') #| imu.Resize((256,256))
+        | imu.ExtractObjects(bgcolor=255) | imu.ShowImage('source')
     img1 = pipe(None)
 
-    # pipe = imu.Pipe() | imu.LoadFile('./images/3003/3003_1840.png') \
-    #     | imu.EqualizeLuminosity() | imu.ExtractObjects(bgcolor=237) #| imu.Resize((256,256))
-    # img2 = pipe(None)
-    # imshow(img2, 'image2')
+    pipe = imu.Pipe() | imu.LoadFile('./images/3003/3003_2030.png') \
+        | imu.ExtractObjects(bgcolor=237) | imu.ShowImage('best match')
+    img2 = pipe(None)
 
-    # return
+    aligned = imu.align_images(img1, img2, mode='euclidian', pad_color=(237, 237, 237))
+    imu.ShowImage()(aligned, 'align')
+
+    score, diff = imu.get_image_diff(img1, aligned, align_mode=None, multichannel=False)
+    print(score)
+    imu.ShowImage()(diff, 'diff')
+    return
 
     files = []
     for d in ['3003', '3070', '3004']:
@@ -23,15 +28,14 @@ def main():
     print(f'{len(files)} files loaded')
 
     scores = []
-    pipe2 = imu.Pipe() | imu.LoadFile(None) | imu.ExtractObjects(bgcolor=237) #| imu.Resize((256,256))
+    pipe2 = imu.Pipe() | imu.LoadFile(None) | imu.ExtractObjects(bgcolor=237)
 
-    # file_subset = random.sample(files, 10)
-    file_subset = files
-    for f in (pbar := tqdm(file_subset)):
+    # files_subset = random.sample(files, 10)
+    files_subset = files
+    for f in (pbar := tqdm(files_subset)):
         pbar.set_description(Path(f).name)
         pipe2[imu.LoadFile].filename = f
         img2 = pipe2(None)
-        # imshow(img2, f)
         try:
             score, _ = imu.get_image_diff(img1, img2, align_mode='euclidian', multichannel=False, 
                 pad_color=imu.COLOR_WHITE)
@@ -40,12 +44,12 @@ def main():
             scores.append(0)
 
     max_score_idx = np.argmax(scores)
-    fn, score = file_subset[max_score_idx], scores[max_score_idx]
+    fn, score = files_subset[max_score_idx], scores[max_score_idx]
     print(f'Max similarity score {score} detected on {fn}')
 
-    pipe[imu.LoadFile].filename = fn
-    max_score_img = pipe(None)
-    imu.ShowImage()(max_score_img, Path(fn).name)
+    pipe2[imu.LoadFile].filename = fn
+    pipe2 |= imu.ShowImage(Path(fn).name)
+    pipe2(None)
 
 if __name__ == '__main__':
     try:
